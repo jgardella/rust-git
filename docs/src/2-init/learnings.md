0c944dafd4b98589e38306f3b6a9cc852d871349
Jason Gardella <jason.a.gardella@gmail.com>
2024-03-16 14:37:25 +0100

    Add basic init implementation
    Adds a basic implementation of init, just creating the .git folder and
    sub-folders with basic error handling. Still need to add handling for
    various options and other edge cases.

--- docs/src/2-init/learnings.md
+++ docs/src/2-init/learnings.md
@@ -1,2 +1,2 @@
 # Initialization - Learnings
-- 
+- Rust has functions for working with the file system in `std::fs`: https://doc.rust-lang.org/rust-by-example/std_misc/fs.html
--- src/init/init.rs
+++ src/init/init.rs
@@ -1,6 +1,45 @@
+use std::{fs::DirBuilder, path::Path};
 use super::cli::InitArgs;
 
-pub(crate) fn init_repository(args: &InitArgs) -> Result<(), ()> // Not sure yet what we should return. Git returns an int.
+const DEFAULT_GIT_DIR: &str = ".git";
+
+fn create_dirs(_: &InitArgs) -> Result<(), String>
 {
-    Ok(())
+    // TODO: handle config and args
+    let git_dir = Path::new(DEFAULT_GIT_DIR);
+
+    if git_dir.exists() {
+        return Err(format!("{git_dir:#?} already exists"))
+    }
+
+    let objects_dir = git_dir.join("objects");
+    let objects_info_dir = objects_dir.join("info");
+    let objects_pack_dir = objects_dir.join("pack");
+
+    let dirs_to_create = vec![
+        git_dir,
+        objects_dir.as_path(),
+        objects_info_dir.as_path(),
+        objects_pack_dir.as_path()
+    ];
+
+    // TODO: set directory permission based on InitArgs.shared and other configs
+    let dir_builder = DirBuilder::new();
+
+    let dir_create_result: Result<Vec<()>, _> = 
+        dirs_to_create
+        .iter()
+        .map(|path| dir_builder.create(path))
+        .collect();
+
+    return 
+        dir_create_result
+        .map(|_| ())
+        .map_err(|err| err.to_string());
+}
+
+
+pub(crate) fn init_repository(args: &InitArgs) -> Result<(), String> // Not sure yet what we should return. Git returns an int.
+{
+    create_dirs(args)
 }
