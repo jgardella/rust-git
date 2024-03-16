170a3d2a992dc7ead09b635fc83088b02f664e46
Jason Gardella <jason.a.gardella@gmail.com>
2024-03-16 11:35:26 +0100

    Implement config-env base flag

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,22 @@ fn parse_config_override(s: &str) -> Result<(String,String), String> {
             Ok((String::from(s), String::from("true")))
         }
     }
+}
 
+fn parse_config_env(s: &str) -> Result<(String,String), String> {
+    match s.find('=') {
+        Some(eq_idx) => {
+            let env_var =  &s[eq_idx+1..];
+            if env_var.is_empty() {
+                Err(String::from("Expected env_var in config-env"))
+            } else {
+                Ok((String::from(&s[0..eq_idx]), String::from(env_var)))
+            }
+        }
+        None => {
+            Err(String::from("Expected '=' in config-env"))
+        }
+    }
 }
 
 #[derive(Debug, Parser)]
@@ -52,6 +67,29 @@ struct Cli {
     #[arg(short='c', value_parser=parse_config_override)]
     config_overrides: Vec<(String, String)>,
 
+    /// Like -c <name>=<value>, give configuration variable <name> a value,
+    /// where <envvar> is the name of an environment variable from which to
+    /// retrieve the value. Unlike -c there is no shortcut for directly
+    /// setting the value to an empty string, instead the environment
+    /// variable itself must be set to the empty string. It is an error if
+    /// the <envvar> does not exist in the environment.  <envvar> may not
+    /// contain an equals sign to avoid ambiguity with <name> containing
+    /// one.
+    /// 
+    /// This is useful for cases where you want to pass transitory
+    /// configuration options to git, but are doing so on OS’s where other
+    /// processes might be able to read your cmdline (e.g.
+    /// /proc/self/cmdline), but not your environ (e.g.
+    /// /proc/self/environ). That behavior is the default on Linux, but may
+    /// not be on your system.
+    /// 
+    /// Note that this might add security for variables such as
+    /// http.extraHeader where the sensitive information is part of the
+    /// value, but not e.g.  url.<base>.insteadOf where the sensitive
+    /// information can be part of the key.
+    #[arg(long, value_parser=parse_config_env)]
+    config_env: Vec<(String, String)>,
+
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -86,5 +124,14 @@ mod tests {
         assert_eq!(parse_config_override("test=blah"), Ok((String::from("test"), String::from("blah"))));
         assert_eq!(parse_config_override("test="), Ok((String::from("test"), String::from(""))));
         assert_eq!(parse_config_override("test"), Ok((String::from("test"), String::from("true"))));
+    }
+
+
+    #[test]
+    fn test_parse_config_env()
+    {
+        assert_eq!(parse_config_env("test=blah"), Ok((String::from("test"), String::from("blah"))));
+        assert_eq!(parse_config_env("test="), Err((String::from("Expected env_var in config-env"))));
+        assert_eq!(parse_config_env("test"), Err((String::from("Expected '=' in config-env"))));
     }
 }
