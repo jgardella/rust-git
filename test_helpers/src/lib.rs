cc1ddfda876185fe0c7f24eda54c4254baad6bcd
Jason Gardella <jason.a.gardella@gmail.com>
2024-03-24 19:32:18 +0100

    Add local crate for test helpers

---
+++ test_helpers/src/lib.rs
@@ -0,0 +1,85 @@
+use std::{fs::File, io::{Read, Write}};
+
+use assert_cmd::Command;
+use assert_fs::{assert::PathAssert, fixture::{ChildPath, PathChild}, TempDir};
+use predicates::{boolean::PredicateBooleanExt, prelude::predicate};
+use flate2::read::ZlibDecoder;
+
+pub trait TempDirExt {
+    fn create_test_file(&self, file_name: &str, contents: &[u8]);
+}
+
+impl TempDirExt for TempDir {
+    fn create_test_file(&self, file_name: &str, contents: &[u8]) {
+        let test_file_path = self.child(file_name);
+        let mut test_file = File::create(test_file_path).unwrap();
+        test_file.write_all(contents).unwrap();
+    }
+}
+
+pub struct TestGitRepo {
+    pub temp_dir: TempDir
+}
+
+impl TestGitRepo {
+    pub fn init() -> Self {
+        let temp_dir = assert_fs::TempDir::new().unwrap();
+
+        Command::cargo_bin("rust-git")
+        .unwrap()
+        .arg("init")
+        .current_dir(temp_dir.path())
+        .unwrap();
+
+        TestGitRepo {
+            temp_dir
+        }
+    }
+
+    fn git_dir(&self) -> ChildPath {
+        self.temp_dir.child(".git")
+    }
+
+    pub fn objects_dir(&self) -> ChildPath {
+        self.git_dir().child("objects")
+    }
+
+    pub fn assert_obj_file(&self, obj_id: &str, contents: &str) {
+        let (folder_name, file_name) = obj_id.split_at(2);
+
+        let obj_file_path = self.objects_dir().child(folder_name).child(file_name);
+        obj_file_path.assert(predicate::path::exists());
+
+        let mut obj_file = File::open(obj_file_path).unwrap();
+        let obj_file_contents = Self::decompress_object_file(&mut obj_file);
+        assert_eq!(contents, obj_file_contents);
+    }
+
+    pub fn assert_no_obj_file(&self, obj_id: &str) {
+        let (folder_name, file_name) = obj_id.split_at(2);
+
+        let obj_file_path = self.objects_dir().child(folder_name).child(file_name);
+        obj_file_path.assert(predicate::path::exists().not());
+    }
+
+    pub fn assert_unsupported_option(&self, command: &str, args: Vec<&str>) {
+        let option = args.first().unwrap();
+
+        Command::cargo_bin("rust-git")
+        .unwrap()
+        .arg(command)
+        .args(&args)
+        .current_dir(self.temp_dir.path())
+        .write_stdin("test")
+        .assert()
+        .failure()
+        .stderr(format!("{option} not supported\n"));
+    }
+
+    fn decompress_object_file(file: &mut File) -> String {
+        let mut decoder = ZlibDecoder::new(file);
+        let mut decoded = String::new();
+        decoder.read_to_string(&mut decoded).unwrap();
+        decoded
+    }
+}
--- tests/hash-object.rs
+++ tests/hash-object.rs
@@ -1,50 +1,12 @@
 #[cfg(test)]
 mod integration_tests {
-    use std::{fs::File, io::Write};
 
-    use assert_fs::{assert::PathAssert, fixture::PathChild, TempDir};
-    use predicates::prelude::*;
     use assert_cmd::{Command, prelude::OutputAssertExt};
-
-    fn create_test_file(temp_dir: &TempDir, file_name: &str, contents: &[u8]) {
-        let test_file_path = temp_dir.child(file_name);
-        let mut test_file = File::create(test_file_path).unwrap();
-        test_file.write_all(contents).unwrap();
-    }
+    use test_helpers::{TempDirExt, TestGitRepo};
 
     #[test]
-    fn should_return_expected_hash_from_stdin() {
-        let temp_dir = assert_fs::TempDir::new().unwrap();
-
-        Command::cargo_bin("rust-git")
-        .unwrap()
-        .arg("init")
-        .current_dir(temp_dir.path())
-        .unwrap();
-
-        let cmd = 
-            Command::cargo_bin("rust-git")
-            .unwrap()
-            .arg("hash-object")
-            .arg("--stdin")
-            .current_dir(temp_dir.path())
-            .write_stdin("test")
-            .unwrap();
-
-        cmd.assert()
-            .success()
-            .stdout("30d74d258442c7c65512eafab474568dd706c430\n");
-    }
-
-    #[test]
-    fn should_write_object_file() {
-        let temp_dir = assert_fs::TempDir::new().unwrap();
-
-        Command::cargo_bin("rust-git")
-        .unwrap()
-        .arg("init")
-        .current_dir(temp_dir.path())
-        .unwrap();
+    fn should_return_and_write_expected_hash_from_stdin() {
+        let test_git_repo = TestGitRepo::init();
 
         let cmd = 
             Command::cargo_bin("rust-git")
@@ -52,7 +14,7 @@ mod integration_tests {
             .arg("hash-object")
             .arg("--stdin")
             .arg("-w")
-            .current_dir(temp_dir.path())
+            .current_dir(test_git_repo.temp_dir.path())
             .write_stdin("test")
             .unwrap();
 
@@ -60,70 +22,89 @@ mod integration_tests {
             .success()
             .stdout("30d74d258442c7c65512eafab474568dd706c430\n");
 
-        let objects_dir = temp_dir.child(".git/objects");
-        let obj_file_folder = objects_dir.child("30");
-
-        obj_file_folder.assert(predicate::path::exists());
-
-        let obj_file = obj_file_folder.child("d74d258442c7c65512eafab474568dd706c430");
-        obj_file.assert(predicate::path::exists());
+        test_git_repo.assert_obj_file("30d74d258442c7c65512eafab474568dd706c430", "blob 4\0test");
     }
 
     #[test]
-    fn should_return_expected_hash_from_files() {
-        let temp_dir = assert_fs::TempDir::new().unwrap();
-
-        let test_file_name1 = "test-file-1.txt";
-        create_test_file(&temp_dir, test_file_name1, b"test1");
-        let test_file_name2 = "test-file-2.txt";
-        create_test_file(&temp_dir, test_file_name2, b"test2");
-
-        Command::cargo_bin("rust-git")
-        .unwrap()
-        .arg("init")
-        .current_dir(temp_dir.path())
-        .unwrap();
+    fn should_not_write_if_flag_missing() {
+        let test_git_repo = TestGitRepo::init();
 
         let cmd = 
             Command::cargo_bin("rust-git")
             .unwrap()
             .arg("hash-object")
+            .arg("--stdin")
+            .current_dir(test_git_repo.temp_dir.path())
+            .write_stdin("test")
+            .unwrap();
+
+        cmd.assert()
+            .success()
+            .stdout("30d74d258442c7c65512eafab474568dd706c430\n");
+
+        test_git_repo.assert_no_obj_file("30d74d258442c7c65512eafab474568dd706c430");
+    }
+
+    #[test]
+    fn should_return_and_write_expected_hash_from_files() {
+        let test_git_repo = TestGitRepo::init();
+
+        let test_file_name1 = "test-file-1.txt";
+        test_git_repo.temp_dir.create_test_file(test_file_name1, b"test1");
+        let test_file_name2 = "test-file-2.txt";
+        test_git_repo.temp_dir.create_test_file(test_file_name2, b"test2");
+
+        let cmd = 
+            Command::cargo_bin("rust-git")
+            .unwrap()
+            .arg("hash-object")
+            .arg("-w")
             .arg(test_file_name1)
             .arg(test_file_name2)
-            .current_dir(temp_dir.path())
+            .current_dir(test_git_repo.temp_dir.path())
             .unwrap();
 
         cmd.assert()
             .success()
             .stdout("f079749c42ffdcc5f52ed2d3a6f15b09307e975e\nd606037cb232bfda7788a8322492312d55b2ae9d\n");
+
+        test_git_repo.assert_obj_file("f079749c42ffdcc5f52ed2d3a6f15b09307e975e", "blob 5\0test1");
+        test_git_repo.assert_obj_file("d606037cb232bfda7788a8322492312d55b2ae9d", "blob 5\0test2");
     }
 
     #[test]
-    fn should_return_expected_hash_from_stdin_paths() {
-        let temp_dir = assert_fs::TempDir::new().unwrap();
+    fn should_return_and_write_expected_hash_from_stdin_paths() {
+        let test_git_repo = TestGitRepo::init();
 
         let test_file_name1 = "test-file-1.txt";
-        create_test_file(&temp_dir, test_file_name1, b"test1");
+        test_git_repo.temp_dir.create_test_file(test_file_name1, b"test1");
         let test_file_name2 = "test-file-2.txt";
-        create_test_file(&temp_dir, test_file_name2, b"test2");
-
-        Command::cargo_bin("rust-git")
-        .unwrap()
-        .arg("init")
-        .current_dir(temp_dir.path())
-        .unwrap();
+        test_git_repo.temp_dir.create_test_file(test_file_name2, b"test2");
 
         let cmd = 
             Command::cargo_bin("rust-git")
             .unwrap()
             .arg("hash-object")
+            .arg("-w")
             .arg("--stdin-paths")
             .write_stdin(format!("{test_file_name1}\n{test_file_name2}"))
-            .current_dir(temp_dir.path())
+            .current_dir(test_git_repo.temp_dir.path())
             .unwrap();
 
         cmd.assert()
             .success()
             .stdout("f079749c42ffdcc5f52ed2d3a6f15b09307e975e\nd606037cb232bfda7788a8322492312d55b2ae9d\n");
+
+        test_git_repo.assert_obj_file("f079749c42ffdcc5f52ed2d3a6f15b09307e975e", "blob 5\0test1");
+        test_git_repo.assert_obj_file("d606037cb232bfda7788a8322492312d55b2ae9d", "blob 5\0test2");
+    }
+
+    #[test]
+    fn should_fail_if_unsupported_options_provided() {
+        let test_git_repo = TestGitRepo::init();
+
+        test_git_repo.assert_unsupported_option("hash-object", vec!["--no-filters"]);
+        test_git_repo.assert_unsupported_option("hash-object", vec!["--path", "my-path"]);
+        test_git_repo.assert_unsupported_option("hash-object", vec!["--literally"]);
     }
  }
