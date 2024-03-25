105ca02121b53f1fe29a884e05c987fe81206daa
Jason Gardella <jason.a.gardella@gmail.com>
2024-03-25 13:21:29 +0100

    Align cat-file CLI parsing with C git

--- src/cat_file/cli.rs
+++ src/cat_file/cli.rs
@@ -3,6 +3,28 @@ use clap::Args;
 use crate::repo::ObjectType;
 
 #[derive(Args, Debug)]
+#[group(multiple=false, requires="object")]
+pub(crate) struct CatFileMode {
+    /// Instead of the content, show the object type identified by <object>.
+    #[arg(short('t'))]
+    show_type: bool,
+
+    /// Instead of the content, show the object size identified by <object>.
+    #[arg(short('s'))]
+    show_size: bool,
+
+    /// Exit with zero status if <object> exists and is a valid object. If <object> is of an invalid format exit with
+    /// non-zero and emits an error on stderr.
+    #[arg(short('e'))]
+    check: bool,
+
+    /// Pretty-print the contents of <object> based on its type.
+    #[arg(short)]
+    print: bool,
+
+}
+
+#[derive(Args, Debug)]
 #[command(about = "Provide content or type and size information for repository objects")]
 #[command(long_about = "
 In its first form, the command provides the content or the type of an object in the repository. The type is
@@ -15,30 +37,22 @@ either --textconv or --filters was specified, the input is expected to list the 
 name, separated by a single whitespace, so that the appropriate drivers can be determined.
 ")]
 pub(crate) struct CatFileArgs {
-    /// Instead of the content, show the object type identified by <object>.
-    #[arg(short('t'), group="mode")]
-    show_type: bool,
+    #[command(flatten)]
+    mode: CatFileMode,
 
-    /// Instead of the content, show the object size identified by <object>.
-    #[arg(short('s'), group="mode")]
-    show_size: bool,
+    // TODO: We are parsing the type and object as a vector; I couldn't find a better way to
+    // represent the way C git handles the cat-file command using Clap.
 
-    /// Exit with zero status if <object> exists and is a valid object. If <object> is of an invalid format exit with
-    /// non-zero and emits an error on stderr.
-    #[arg(short('e'), group="mode")]
-    check: bool,
-
-    /// Pretty-print the contents of <object> based on its type.
-    #[arg(short, group="mode")]
-    print: bool,
-
+    /// <type> 
+    /// 
     /// Typically this matches the real type of <object> but asking for a type that can trivially be dereferenced from
     /// the given <object> is also permitted. An example is to ask for a "tree" with <object> being a commit object
     /// that contains it, or to ask for a "blob" with <object> being a tag object that points at it.
-    #[arg(value_name="type")]
-    obj_type: Option<ObjectType>,
-
+    /// 
+    /// <object>
+    /// 
     /// The name of the object to show. For a more complete list of ways to spell object names, see the "SPECIFYING
     /// REVISIONS" section in gitrevisions(7).
-    object: Option<String>,
+    #[arg(value_names=(["type", "object"]), num_args=0..3)]
+    object: Vec<String>,
 }
