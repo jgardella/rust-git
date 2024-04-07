fb90e3449c8515c8bf973f72b02bcb2a724987fa
Jason Gardella <jason.a.gardella@gmail.com>
2024-04-07 09:58:43 +0200

    Start implementing mv command

--- src/mv/command.rs
+++ src/mv/command.rs
@@ -1,5 +1,5 @@
 
-use std::fs;
+use std::{fs, path::{Path, PathBuf}};
 
 use crate::{command::GitCommand, repo::RepoState, RustGitError};
 
@@ -7,8 +7,6 @@ use super::cli::MvArgs;
 
 pub(crate) struct MvCommand {
     args: MvArgs,
-
-    // TODO: add base args
 }
 
 impl MvCommand {
@@ -19,10 +17,45 @@ impl MvCommand {
     }
 }
 
+enum MvAction {
+    Move { source: Vec<PathBuf>, destination: PathBuf },
+    Rename { source: PathBuf, destination: PathBuf },
+}
+
+fn get_action(files: &Vec<String>) -> Result<MvAction, RustGitError> {
+    match files.as_slice() {
+        [] => Err(RustGitError::new("mv expects at least 2 inputs, got 0")),
+        [_] => Err(RustGitError::new("mv expects at least 2 inputs, got 1")),
+        [source, destination] => {
+            // If the last input provided is an existing directory,
+            // we assume it's a move. Otherwise, it's a rename.
+            match fs::metadata(Path::new(destination)) {
+                Ok(metadata) if metadata.is_dir() => 
+                    Ok(MvAction::Move { source: vec![PathBuf::from(source)], destination: PathBuf::from(destination) }),
+                _ => 
+                    Ok(MvAction::Rename { source: PathBuf::from(source), destination: PathBuf::from(destination) }),
+            }
+        },
+        [source @ .., destination] => {
+            // Must be a move as only move supports 3 or more arguments.
+            Ok(MvAction::Move { source: source.iter().map(PathBuf::from).collect(), destination: PathBuf::from(destination) })
+        }
+    }
+}
+
 impl GitCommand for MvCommand {
     fn execute(&self, repo_state: RepoState) -> Result<(), RustGitError>
     {
         let mut repo = repo_state.try_get()?;
+
+        let last_file = self.args.files.last().unwrap();
+
+        match get_action(&self.args.files)? {
+            MvAction::Move { source, destination } => todo!(),
+            MvAction::Rename { source, destination } => todo!(),
+        }
+
+        fs::
 
         Ok(())
     }
