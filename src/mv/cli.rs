c1d758f1bb081b7e65f8c393860751171888095a
Jason Gardella <jason.a.gardella@gmail.com>
2024-04-07 09:58:43 +0200

    Start implementing mv command

--- src/mv/cli.rs
+++ src/mv/cli.rs
@@ -14,24 +14,24 @@ sources will be moved into this directory.
 
 The index is updated after successful completion, but the change must still be committed.")]
 pub(crate) struct MvArgs {
-    #[arg(num_args(1..))]
-    files: Vec<String>,
+    #[arg(num_args(2..))]
+    pub files: Vec<String>,
 
     /// Force renaming or moving of a file even if the target exists
     #[arg(short, long)]
-    force: bool,
+    pub force: bool,
 
     /// Skip move or rename actions which would lead to an error condition. An error happens when a source
     /// is neither existing nor controlled by Git, or when it would overwrite an existing file unless -f
     /// is given.
     #[arg(short('k'))]
-    skip: bool,
+    pub skip: bool,
 
     /// Do nothing; only show what would happen
     #[arg(short('n'), long)]
-    dry_run: bool,
+    pub dry_run: bool,
 
     /// Report the names of files as they are moved.
     #[arg(short, long)]
-    verbose: bool,
+    pub verbose: bool,
 }
