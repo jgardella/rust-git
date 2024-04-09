d0d12aa017594e7ed3c980f76ab6f88eb3f2a746
Jason Gardella <jason.a.gardella@gmail.com>
2024-04-09 10:34:42 +0200

    Add integration tests for restore command

---
+++ tests/restore.rs
@@ -0,0 +1,57 @@
+mod integration_tests {
+    use std::io::Write;
+
+    use assert_cmd::{assert::OutputAssertExt, Command};
+    use assert_fs::{assert::PathAssert, fixture::PathChild};
+    use test_helpers::{TempDirExt, TestGitRepo};
+
+    #[test]
+    fn should_restore_file_from_index() {
+        let test_git_repo = TestGitRepo::new();
+        let mut test_file = test_git_repo.temp_dir.create_test_file("test.txt", b"original contents");
+
+        test_git_repo.init();
+        test_git_repo.add("test.txt");
+
+        test_file.write(b"updated file").unwrap();
+
+        let cmd = 
+            Command::cargo_bin("rust-git")
+            .unwrap()
+            .arg("restore")
+            .arg("test.txt")
+            .current_dir(test_git_repo.temp_dir.path())
+            .unwrap();
+
+        cmd.assert().success();
+
+        test_git_repo.temp_dir.child("test.txt").assert("original contents");
+    }
+
+    #[test]
+    fn should_restore_directory_from_index() {
+        let test_git_repo = TestGitRepo::new();
+        test_git_repo.temp_dir.create_test_dir("test_dir");
+        let mut test_file1 = test_git_repo.temp_dir.create_test_file("test_dir/test.txt", b"original contents");
+        let mut test_file2 = test_git_repo.temp_dir.create_test_file("test_dir/test2.txt", b"original contents 2");
+
+        test_git_repo.init();
+        test_git_repo.add("test_dir");
+
+        test_file1.write(b"updated file 1").unwrap();
+        test_file2.write(b"updated file 2").unwrap();
+
+        let cmd = 
+            Command::cargo_bin("rust-git")
+            .unwrap()
+            .arg("restore")
+            .arg("test_dir")
+            .current_dir(test_git_repo.temp_dir.path())
+            .unwrap();
+
+        cmd.assert().success();
+
+        test_git_repo.temp_dir.child("test_dir/test.txt").assert("original contents");
+        test_git_repo.temp_dir.child("test_dir/test2.txt").assert("original contents 2");
+    }
+}
